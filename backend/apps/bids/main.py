7e107238304f7879155b8d4f4ae4a57df447af74
guillermo-tellRM <guillermotellg@gmail.com>
2023-10-25 13:34:26 +0200

    ✨ Created connection with database on bids service

--- backend/apps/bids/main.py
+++ backend/apps/bids/main.py
@@ -1,19 +1,39 @@
 from typing import Union
-
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
 
-import pymongo
+import bidModel
+from bidModel import Bid
+
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
-load_dotenv()
-uri = f"mongodb+srv://{os.getenv('MONGODB_USER')}:{os.getenv('MONGODB_PASSWORD')}{os.getenv('MONGODB_CLUSTER')}.mongodb.net/?retryWrites=true&w=majority"
+# load_dotenv()
+uri = f"mongodb+srv://{os.getenv('MONGODB_USER')}:{os.getenv('MONGODB_PASSWORD')}@{os.getenv('MONGODB_CLUSTER')}.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# Set the desired db
+db = client.elRastro
+
+# Show collections
+print(db.list_collection_names())
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
