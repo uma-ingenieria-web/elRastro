a73971059f6a791807ae217fa877aeca4d3aef87
Javier Jordán Luque <javierjordanluque@gmail.com>
2023-10-31 20:42:27 +0100

    🎨 Estructura de Usuario

--- backend/apps/users/userModel.py
+++ backend/apps/users/userModel.py
@@ -1,6 +1,7 @@
 import pymongo
 from bson import ObjectId
 from pydantic import BaseModel, Field
+from typing import List
 
 class PyObjectId(ObjectId):
     @classmethod
@@ -17,11 +18,20 @@ class PyObjectId(ObjectId):
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
 
-class UserModel(BaseModel):
+class Product(BaseModel):
+    id: str = Field(...)
+    name: str = Field(...)
+
+class Bid(BaseModel):
+    id: str = Field(...)
+    amount: float = Field(...)
+    product: Product = Field(...)
+
+class User(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     username: str = Field(...)
-    products: list = Field(...)
-    bids: list = Field(...)
+    products: List[Product]
+    bids: List[Bid]
 
     class Config:
         allow_population_by_field_name = True
@@ -32,14 +42,18 @@ class UserModel(BaseModel):
                 "username": "Javi Jordan",
                 "products": [
                     {
-                        "_id": "productId",
-                        "name": "balon"
+                        "_id": "653e27ba54d16794592d4731",
+                        "name": "Balon de Futbol"
                     }
                 ],
                 "bids": [
                     {
-                        "_id": "productId",
-                        "amount": "20"
+                        "_id": "653e27ba54d16794592d4741",
+                        "amount": 550.0,
+                        "product": {
+                            "_id": "653e27ba54d16794592d4751",
+                            "name": "Ordenador Portatil"
+                        }
                     }
                 ]
             }
