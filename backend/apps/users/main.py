3df29f7260707242283ce63f9dcc2c1741ee57c5
José Manuel <115642154+josesanc02@users.noreply.github.com>
2023-11-01 10:06:03 +0100

    ✨ Creating a user route

--- backend/apps/users/main.py
+++ backend/apps/users/main.py
@@ -1,20 +1,36 @@
 from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, HTTPException, status
 from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
 
-import pymongo
+import os
+import userModel
 
 app = FastAPI()
 
 load_dotenv()
-uri = f"mongodb+srv://{os.getenv('MONGODB_USER')}:{os.getenv('MONGODB_PASSWORD')}{os.getenv('MONGODB_CLUSTER')}.mongodb.net/?retryWrites=true&w=majority"
+
+# load_dotenv()
+uri = os.getenv('MONGODB_URI')
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Set the desired db
+db = client.elRastro.User
 
 versionRoute = "api/v1"
 
 @app.get("/" + versionRoute + "/users/{user_id}")
 async def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/" + versionRoute + "/users",
+        status_code=status.HTTP_201_CREATED,)
+async def create_user(user: userModel.CreateUser = Body(...)):
+    db.insert_one(user.model_dump(by_alias=True, exclude=["id"]))
+    
 
 
 @app.get("/items/{item_id}")
