f5f73094a59ba0f5b45d5a01f47e09c137578cbc
José Manuel <115642154+josesanc02@users.noreply.github.com>
2023-11-01 10:37:37 +0100

    ✨ Find a user with a given id

--- backend/apps/users/main.py
+++ backend/apps/users/main.py
@@ -1,6 +1,6 @@
-from typing import Union
 from fastapi import FastAPI, Body, HTTPException, status
 from dotenv import load_dotenv
+from bson import ObjectId
 from pymongo.mongo_client import MongoClient
 
 import os
@@ -21,18 +21,22 @@ db = client.elRastro.User
 
 versionRoute = "api/v1"
 
-@app.get("/" + versionRoute + "/users/{user_id}")
-async def read_root():
-    return {"Hello": "World"}
+@app.get("/" + versionRoute + "/user/{id}",
+        response_description="The user with the given id",
+        summary="Find a user with a certain id",
+        status_code=status.HTTP_200_OK,)
+async def read_user(id: str):
+    user = db.find_one({"_id": ObjectId(id)})
+    
+    if user is not None:
+        user['_id'] = str(user['_id'])
+        return user
+    else:
+        raise HTTPException(status_code=404, detail=f"User {id} not found")
 
 
-@app.post("/" + versionRoute + "/users",
+@app.post("/" + versionRoute + "/user",
         status_code=status.HTTP_201_CREATED,)
 async def create_user(user: userModel.CreateUser = Body(...)):
     db.insert_one(user.model_dump(by_alias=True, exclude=["id"]))
     
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
