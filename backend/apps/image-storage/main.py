e70b64823b10db9181562d4aca49931d36be45f2
José Manuel <jmsanchezrico@gmail.com>
2023-11-03 19:10:42 +0100

    ✨ Uploading images on cloudinary microservice

--- backend/apps/image-storage/main.py
+++ backend/apps/image-storage/main.py
@@ -1,15 +1,38 @@
-from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from dotenv import load_dotenv
+
+import os
+import cloudinary
+import cloudinary.uploader as cloudinary_uploader
 
 app = FastAPI()
 
+load_dotenv()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# load_dotenv()
+uri = os.getenv('MONGODB_URI')
+app = FastAPI()
 
+          
+cloudinary.config( 
+  cloud_name = "dnzyzkglp", 
+  api_key = os.getenv("API_KEY"), 
+  api_secret = os.getenv("API_SECRET")
+)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/user/{id}/photo")
+def get_user_photo(id: str):
+    image_url, options = cloudinary.utils.cloudinary_url(id, format="jpeg")
+    return image_url
+
+@app.post("/user/{id}/photo")
+def post_photo(id: str, file: UploadFile = File()):
+    response = cloudinary_uploader.upload(file.file, public_id=id, transformation=[{
+        'width': 100,
+        'height': 100,
+        'crop': 'fill'
+
+    }])
+    return response["secure_url"]
+
