9d13a4820a0d3ac5c8ed77637714c7a9490bf867
guillermo-tellRM <guillermotellg@gmail.com>
2023-11-06 13:07:35 +0100

    ✨ Set up for carbon-footprint microservice

--- backend/apps/carbon-footprint/main.py
+++ backend/apps/carbon-footprint/main.py
@@ -1,15 +1,10 @@
-from typing import Union
-
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+import os
 
 app = FastAPI()
 
+load_dotenv()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+API_KEY = os.getenv("API_KEY")
