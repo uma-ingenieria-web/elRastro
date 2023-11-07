2e0086650dea92d0a46dab3350163f173f2f631a
José Manuel <jmsanchezrico@gmail.com>
2023-11-07 10:25:36 +0100

    📝 Added documentation on cloudinary methods

--- backend/apps/image-storage/main.py
+++ backend/apps/image-storage/main.py
@@ -21,12 +21,34 @@ cloudinary.config(
   api_secret = os.getenv("API_SECRET")
 )
 
-@app.get("/user/{id}/photo")
+versionRoute = "api/v1"
+
+@app.get("/" + versionRoute + "/user/{id}/photo",
+         description="Get url to photo from the id of a user",
+         responses={
+            200: {
+                "description": "Return the url to the image.",
+                "content": {"application/txt": {
+                    "example": "https://res.cloudinary..."
+                }},
+            }
+        },
+         tags=["User"])
 def get_user_photo(id: str):
     image_url, options = cloudinary.utils.cloudinary_url(id, format="jpeg")
     return image_url
 
-@app.post("/user/{id}/photo")
+@app.post("/" + versionRoute + "/user/{id}/photo",
+        description="Upload a photo with an id",
+        responses={
+            200: {
+                "description": "Return the url to the saved image.",
+                "content": {"application/txt": {
+                    "example": "https://res.cloudinary..."
+                }},
+            }
+        },
+        tags=["User"])
 def post_photo(id: str, file: UploadFile = File()):
     response = cloudinary_uploader.upload(file.file, public_id=id, transformation=[{
         'width': 100,
