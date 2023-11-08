b48f8102aae97eacf29b1bdf0f366017fd063bc9
José Manuel <jmsanchezrico@gmail.com>
2023-11-08 10:43:14 +0100

    📝 Documentation for OpenAPI on routes

---
+++ backend/apps/users/errors.py
@@ -0,0 +1,26 @@
+
+error_400 = {
+    "description": "Invalid ObjectId format",
+    "content": {
+        "application/json": {"example": {"message": "Invalid ObjectId format"}}
+    },
+}
+
+error_404 = {
+    "description": "User not found",
+    "content": {"application/json": {"example": {"message": "User not found"}}},
+}
+
+error_422 = {
+    "description": "Invalid data",
+    "content": {
+        "application/json": {
+            "example": {
+                "detail": [
+                    "Field 'field_name' is required",
+                    "Field 'another_field' is required",
+                ]
+            }
+        }
+    },
+}
