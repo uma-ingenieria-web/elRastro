8a890034db6e6fc71f8214aa6503c016d9c6b6bc
Javier Jordán Luque <javierjordanluque@gmail.com>
2023-11-09 11:15:11 +0100

    🎨 Removed Ping for Equivalent Format

--- backend/apps/bids/main.py
+++ backend/apps/bids/main.py
@@ -21,13 +21,6 @@ uri = os.getenv("MONGODB_URI")
 # Create a new client and connect to the server
 client = MongoClient(uri)
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command("ping")
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
 # Set the desired db
 db = client.elRastro
 
