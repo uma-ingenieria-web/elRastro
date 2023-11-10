5ddcb6d11ba31d0910c328924be9641a74637322
PabloFS <pablofs02@tutanota.com>
2023-11-10 04:08:59 +0100

    🌎 Implementation of get_all_places

--- backend/apps/coord-place/main.py
+++ backend/apps/coord-place/main.py
@@ -31,3 +31,19 @@ def get_first_place(lat: int = None, lon: int = None):
         return {"country": req.json()[0]["country"], "city": req.json()[0]["name"]}
     except:
         raise HTTPException(404, "Not found")
+
+
+# Get all the country and city names from coordinates
+@app.get("/" + versionRoute + "/reversegeocoding",
+            summary="Get all country and city names by coordinates",
+            response_description="Returns the country and city names from coordinates",
+            status_code=status.HTTP_200_OK,
+            responses={404: errors.error_404})
+def get_all_places(lat: int = None, lon: int = None):
+    if lat == None or lon == None:
+        raise HTTPException(status_code=400, detail="Incorrect input")
+    try:
+        req = requests.get(base_url + "?lat=" + str(lat) + "&lon=" + str(lon), headers=headers)
+        return req.json()
+    except:
+        raise HTTPException(404, "Not found")
