e0a43bb0ac672256f4dc3a5dd62e4e6895a7154e
Javier Jordán Luque <javierjordanluque@gmail.com>
2023-11-22 11:53:11 +0100

    ✨ Created query to get usernames from a conversation

--- backend/apps/messages/main.py
+++ backend/apps/messages/main.py
@@ -107,11 +107,11 @@ def send_message(message: CreateMessage, origin: str, destination: str, product_
 
 @app.get("/" + versionRoute + "/conversation",
          summary="Get the conversation between user origin and destination from one product sorted by timestamp",
-         response_description="The the conversation between user origin and destination from one product sorted by timestamp",
+         response_description="The conversation between user origin and destination from one product sorted by timestamp",
          response_model=List[Message],
          status_code=status.HTTP_200_OK,
          responses={400: errors.error_400, 404: errors.error_404, 422: errors.error_422},
-         tags=["User"])
+         tags=["Message"])
 async def get_conversation_sorted_by_timestamp(origin: str, destination: str, product_id: str):
     user_origin = db.User.find_one({"_id": ObjectId(origin)})
     if user_origin is None:
@@ -142,3 +142,74 @@ async def get_conversation_sorted_by_timestamp(origin: str, destination: str, pr
     }).sort("timestamp", 1)
 
     return list(conversation)
+
+
+@app.get("/" + versionRoute + "/conversations/usernames",
+         summary="Get the usernames of the users that have a conversation with user from one product sorted by timestamp",
+         response_description="The usernames of the users that have a conversation with user from one product sorted by timestamp",
+         response_model=List[str],
+         status_code=status.HTTP_200_OK,
+         responses={400: errors.error_400, 404: errors.error_404, 422: errors.error_422},
+         tags=["Message"])
+async def get_conversations_usernames_sorted_by_timestamp(user_id: str, product_id: str):
+    user = db.User.find_one({"_id": ObjectId(user_id)})
+    if user is None:
+        raise HTTPException(status_code=404, detail=f"User could not be found")
+    product = db.Product.find_one({"_id": ObjectId(product_id)})
+    if product is None:
+        raise HTTPException(status_code=404, detail=f"Product could not be found")
+    # Verifica si el producto pertenece al usuario origen o destino
+    product_owned = db.Product.find({
+        "_id": ObjectId(product_id),
+        "owner._id": ObjectId(user_id)
+    })
+    if product_owned is None:
+        raise HTTPException(status_code=404, detail=f"Product is not owned by the user")
+    
+    conversations = db.Message.find(
+            {
+                "$or": [
+                    {"$and": [{"origin._id": ObjectId(user_id)}, {"product._id": ObjectId(product_id)}]},
+                    {"$and": [{"destination._id": ObjectId(user_id)}, {"product._id": ObjectId(product_id)}]}
+                ]
+            },
+            {"origin._id": 1, "destination._id": 1}
+        )
+
+    # Extrae los IDs de usuarios únicos de las conversaciones
+    user_ids = set()
+    for conversation in conversations:
+        if conversation["origin"]["_id"] != ObjectId(user_id):
+            user_ids.add(conversation["origin"]["_id"])
+        if conversation["destination"]["_id"] != ObjectId(user_id):
+            user_ids.add(conversation["destination"]["_id"])
+
+    user_ids_list = list(user_ids)
+    # Obtiene información de usuarios
+    users = db.User.find({"_id": {"$in": user_ids_list}})
+
+    contacts_list = []
+    # Obtiene la conversación ordenada por timestamp descendente
+    for contact in users:
+        messages = db.Message.find({
+            "$or": [
+                {"$and": [{"origin._id": ObjectId(user_id)}, {"destination._id": contact["_id"]}, {"product._id": ObjectId(product_id)}]},
+                {"$and": [{"origin._id": contact["_id"]}, {"destination._id": ObjectId(user_id)}, {"product._id": ObjectId(product_id)}]}
+            ]
+        }).sort("timestamp", -1).limit(1)
+    
+        if messages:
+            latest_message = messages[0]
+            
+        contacts_list.append({
+            "username": contact["username"],
+            "latest_timestamp": latest_message["timestamp"] if latest_message else None
+        })
+
+    # Ordena la lista por timestamp descendente
+    contacts_list = sorted(contacts_list, key=lambda x: x["latest_timestamp"], reverse=True)
+    
+    # Obtiene solo los usernames ordenados
+    sorted_usernames = [contact["username"] for contact in contacts_list]
+    
+    return sorted_usernames
