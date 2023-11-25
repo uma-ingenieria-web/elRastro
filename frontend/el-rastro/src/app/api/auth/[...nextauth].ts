3111a305670ef2abcfc761198d70f4d5ac7ea590
José Manuel <jmsanchezrico@gmail.com>
2023-11-25 12:25:39 +0100

    🔒️ Added Google authentication file

---
+++ frontend/el-rastro/src/app/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+if (!process.env.GOOGLE_ID || !process.env.GOOGLE_SECRET)
+{
+  throw new Error("GOOGLE environment variables not set");
+}
+
+export const authOptions : AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+export default NextAuth(authOptions);
