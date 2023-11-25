8568a1467c46a5f43d9596bd7512fa0ff56a8666
guillermo-tellRM <guillermotellg@gmail.com>
2023-11-25 14:00:23 +0100

    🐛 Now using Link

--- frontend/el-rastro/src/app/components/ProductCard.tsx
+++ frontend/el-rastro/src/app/components/ProductCard.tsx
@@ -16,14 +16,14 @@ function ProductCard(product: Product) {
   return (
     <div className="w-full max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <Link href="#">
-        <img className="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
+        {/* <img className="p-8 rounded-t-lg" src="" alt="product image" /> */}
     </Link>
       <div className="px-5 pb-5">
-        <a href="#">
+        <Link href="#">
           <h5 className="text-3xl font-semibold tracking-tight text-gray-900 dark:text-white">
             {product.title}
           </h5>
-        </a>
+        </Link>
         <div className="flex items-center mt-2.5 mb-5">
           <div className="flex items-center space-x-1 rtl:space-x-reverse">
             <svg
@@ -44,12 +44,11 @@ function ProductCard(product: Product) {
           <span className="text-3xl font-bold text-gray-900 dark:text-white">
             {product.initialPrice}
           </span>
-          <a
-            href="#"
+          <button
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Bid
-          </a>
+          </button>
         </div>
       </div>
     </div>
