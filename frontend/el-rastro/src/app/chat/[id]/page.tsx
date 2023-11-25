d44f5787815e86765205adc8cf65c5729efc39ba
Javier Jordán Luque <javierjordanluque@gmail.com>
2023-11-25 13:25:59 +0100

    ✨ chat/[id] terminado

--- frontend/el-rastro/src/app/chat/[id]/page.tsx
+++ frontend/el-rastro/src/app/chat/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useState } from 'react';
 
 interface Message {
@@ -19,7 +19,9 @@ interface ChatInfo {
   interested: {
     _id: string;
   };
-  product_id: string;
+  product: {
+    _id: string;
+  };
 }
 
 interface ProductInfo {
@@ -33,6 +35,7 @@ export default function ChatPageId({ params }: { params: { id: string } }) {
   const [productInfo, setProductInfo] = useState<ProductInfo | null>(null);
   const [loading, setLoading] = useState(true);
   const [newMessageText, setNewMessageText] = useState('');
+  const [isSendButtonDisabled, setIsSendButtonDisabled] = useState(true);
 
   useEffect(() => {
     fetchData();
@@ -40,24 +43,20 @@ export default function ChatPageId({ params }: { params: { id: string } }) {
 
   const fetchData = async () => {
     try {
-      // Marcador de posición para la carga de datos existente
-      // Reemplaza esta parte con tu lógica para cargar datos
       const result = await fetch(`http://localhost:8008/api/v1/chat/${id}`);
       const chatData = await result.json();
       setChatInfo(chatData);
-      console.log('Chat Data:', chatData);
 
       const messagesResult = await fetch(`http://localhost:8008/api/v1/chat/${id}/messages`);
       const messagesData = await messagesResult.json();
       setMessages(messagesData);
-      console.log('Messages Data:', messagesData);
 
-      const productResult = await fetch(`http://localhost:8002/api/v1/products/${chatData.product_id}`);
+      const productResult = await fetch(`http://localhost:8002/api/v1/products/${chatData.product._id}`);
       const productData = await productResult.json();
       setProductInfo(productData);
-      console.log('Product Data:', productData);
 
-      setNewMessageText(''); // Limpiar el campo de texto después de cargar los datos
+      setNewMessageText('');
+      setIsSendButtonDisabled(true);
     } catch (error) {
       console.error('Error fetching data:', error);
     } finally {
@@ -67,6 +66,10 @@ export default function ChatPageId({ params }: { params: { id: string } }) {
 
   const handleSendMessage = async () => {
     try {
+      if (!newMessageText.trim()) {
+        return;
+      }
+
       const response = await fetch(`http://localhost:8008/api/v1/chat/${id}/send?origin_id=${chatInfo?.vendor._id}`, {
         method: 'POST',
         headers: {
@@ -76,7 +79,6 @@ export default function ChatPageId({ params }: { params: { id: string } }) {
       });
 
       if (response.ok) {
-        // Recargar los datos después de enviar el mensaje
         fetchData();
       } else {
         console.error('Error al enviar el mensaje');
@@ -86,45 +88,100 @@ export default function ChatPageId({ params }: { params: { id: string } }) {
     }
   };
 
+  function formatMessageDate(timestamp: string) {
+    const date = new Date(timestamp);
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' } as Intl.DateTimeFormatOptions;
+    return date.toLocaleDateString('en-US', options);
+  }
+
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="flex justify-center items-center flex-col h-screen bg-gray-100">
-      {productInfo && (
-        <div style={{ marginBottom: '10px' }}>
-          Conversation for {productInfo.title}
-        </div>
-      )}
-      <div>
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            style={{
-              textAlign: message.origin._id === chatInfo?.vendor._id ? 'left' : 'right',
-              padding: '5px',
-              border: '1px solid #ccc',
-              margin: '5px',
-              borderRadius: '8px',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: message.origin._id === chatInfo?.vendor._id ? 'flex-start' : 'flex-end',
-            }}
-          >
-            <div>{message.text}</div>
-            <div style={{ fontSize: '0.8em', color: '#888' }}>{message.timestamp}</div>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div style={{ width: '500px', border: '1px solid #ccc', borderRadius: '8px', overflow: 'hidden', display: 'flex', flexDirection: 'column', height: '600px' }}>
+        {productInfo && (
+          <div style={{ padding: '10px', borderBottom: '1px solid #ccc', backgroundColor: '#f5f5f5' }}>
+            Conversation for: {productInfo.title}
           </div>
-        ))}
-      </div>
-      <div>
-        <input
-          type="text"
-          value={newMessageText}
-          onChange={(e) => setNewMessageText(e.target.value)}
-          placeholder="Type your message..."
-        />
-        <button onClick={handleSendMessage}>Send</button>
+        )}
+        <div style={{ display: 'flex', flexDirection: 'column', height: '100%', position: 'relative' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-between', padding: '5px', fontWeight: 'bold' }}>
+            <div style={{ width: '50%', textAlign: 'center' }}>VENDOR</div>
+            <div style={{ width: '50%', textAlign: 'center' }}>INTERESTED</div>
+          </div>
+          <div style={{ overflowY: 'auto', flex: '1', position: 'relative', maxHeight: '80%' }}>
+            <table style={{ width: '100%', tableLayout: 'fixed', borderCollapse: 'collapse' }}>
+              <tbody>
+                {messages.map((message, index) => (
+                  <tr key={index}>
+                    <td style={{ textAlign: 'left', padding: '5px', wordWrap: 'break-word' }}>
+                      {message.origin._id === chatInfo?.vendor._id && (
+                        <div
+                          style={{
+                            border: '1px solid #ccc',
+                            borderRadius: '8px',
+                            padding: '5px',
+                            marginBottom: '5px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                          }}
+                        >
+                          <div>{message.text}</div>
+                          <div style={{ fontSize: '0.8em', color: '#888' }}>{formatMessageDate(message.timestamp)}</div>
+                        </div>
+                      )}
+                    </td>
+                    <td style={{ textAlign: 'right', padding: '5px', wordWrap: 'break-word' }}>
+                      {message.origin._id === chatInfo?.interested._id && (
+                        <div
+                          style={{
+                            border: '1px solid #ccc',
+                            borderRadius: '8px',
+                            padding: '5px',
+                            marginBottom: '5px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                          }}
+                        >
+                          <div>{message.text}</div>
+                          <div style={{ fontSize: '0.8em', color: '#888' }}>{formatMessageDate(message.timestamp)}</div>
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div style={{ padding: '10px', borderTop: '1px solid #ccc', backgroundColor: '#f5f5f5', display: 'flex', position: 'absolute', bottom: '0', width: '100%' }}>
+            <input
+              type="text"
+              value={newMessageText}
+              onChange={(e) => {
+                setNewMessageText(e.target.value);
+                setIsSendButtonDisabled(!e.target.value.trim());
+              }}
+              placeholder="Type your message..."
+              style={{ width: 'calc(100% - 70px)', marginRight: '10px' }}
+            />
+            <button
+              onClick={handleSendMessage}
+              disabled={isSendButtonDisabled}
+              style={{
+                padding: '5px 10px',
+                backgroundColor: '#4CAF50',
+                color: 'white',
+                border: 'none',
+                borderRadius: '4px',
+                cursor: 'pointer',
+              }}
+            >
+              Send
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
