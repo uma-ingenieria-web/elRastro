f136daa16358e330c42428c4a136c954a51eaf9e
guillermo-tellRM <guillermotellg@gmail.com>
2023-12-02 13:52:08 +0100

    🐛 Fix styling in Filter and ProductCard

--- frontend/el-rastro/src/app/components/Filter.tsx
+++ frontend/el-rastro/src/app/components/Filter.tsx
@@ -90,7 +90,7 @@ function Filter() {
             >
               <path d="M18.85 1.1A1.99 1.99 0 0 0 17.063 0H2.937a2 2 0 0 0-1.566 3.242L6.99 9.868 7 14a1 1 0 0 0 .4.8l4 3A1 1 0 0 0 13 17l.01-7.134 5.66-6.676a1.99 1.99 0 0 0 .18-2.09Z" />
             </svg>
-            <span className="hidden sm:inline">Show Filters</span>
+            <span className="hidden sm:inline ">Show Filters</span>
           </div>
         </button>
       </div>
--- frontend/el-rastro/src/app/components/ProductCard.tsx
+++ frontend/el-rastro/src/app/components/ProductCard.tsx
@@ -21,7 +21,7 @@ function ProductCard(product: ProductInterface) {
   }
 
   return (
-    <div className="w-full max-w-lg h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex flex-col justify-between w-full max-w-lg h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <Link href={"/product/" + product._id}>
         <img
           className="mb-3 rounded-t-lg"
@@ -29,7 +29,7 @@ function ProductCard(product: ProductInterface) {
           alt="product image"
         />
       </Link>
-      <div className="px-5 pb-5">
+      <div className="px-5 pb-5 flex flex-col justify-between flex-grow">
         <div className="flex flex-row justify-between items-center md:items-start mt-2">
           <Link href={"/product/" + product._id}>
             <h5
@@ -76,10 +76,6 @@ function ProductCard(product: ProductInterface) {
                       </p>
                       <div></div>
                     </div>
-                    <p className="mt-5 text-sm">
-                      Number of products sold:{" "}
-                      {/* Add number of products sold */}
-                    </p>
                     <div className="flex text-sm">{/* Add rating */}</div>
                   </div>
                 </div>
@@ -94,21 +90,21 @@ function ProductCard(product: ProductInterface) {
               : product.description}
           </p>
         </div>
-        <div className="flex justify-between items-end mt-3">
+        <div className="flex justify-between items-center mt-3">
           <div className="text-3xl sm:text-2xl font-bold text-gray-900 dark:text-white">
             {product.bids.length == 0
               ? product.initialPrice
               : product.bids[product.bids.length - 1].amount}
             €
           </div>
-          <div className="ml-4">
+          <div className="ml-4 align-middle">
             {closed ? (
-              <button className="text-black bg-gray-300 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-lg sm:text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Closed
+              <button className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 hover:scale-105 transform transition-all duration-300">
+                More like this
               </button>
             ) : (
               <Link href={`/product/${product._id}`}>
-                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 hover:scale-105 transform transition-all duration-300">
                   Show more
                 </button>
               </Link>
