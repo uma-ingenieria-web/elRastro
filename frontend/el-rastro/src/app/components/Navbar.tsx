219292dfaefd4a2410131d3f4bcf436ef496694f
José Manuel <jmsanchezrico@gmail.com>
2023-12-02 19:52:53 +0100

    💄 Fix gradient on body to white background

--- frontend/el-rastro/src/app/components/Navbar.tsx
+++ frontend/el-rastro/src/app/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const { data: session } = useSession();
     return (
         <>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <nav className="bg-white border border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">El Rastro</span>
@@ -21,7 +21,7 @@ const Navbar = () => {
                         </svg>
                     </button>
                     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-500 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <NavbarItem pathName="/" name="Home" />
                             <NavbarItem pathName="/product" name="Products" />
                             {session?.user ? (
--- frontend/el-rastro/src/app/page.tsx
+++ frontend/el-rastro/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-tr from-white to-slate-200 ">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 ">
       <section className="hero">
         <h1 className="text-8xl font-bold text-center text-black">El Rastro</h1>
         <h2 className="text-lg text-black">The online marketplace for buying and selling products.</h2>
