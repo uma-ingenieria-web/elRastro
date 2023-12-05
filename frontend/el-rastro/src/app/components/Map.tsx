d0fc8f9dbaa3c507ba7d3243f55dbeb0ac8cdadd
guillermo-tellRM <guillermotellg@gmail.com>
2023-12-05 20:53:04 +0100

    🐛 Fixed Map

--- frontend/el-rastro/src/app/components/Map.tsx
+++ frontend/el-rastro/src/app/components/Map.tsx
@@ -1,24 +1,30 @@
-"use client";
-import React from 'react';
+"use client"
+
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+const defaultIconUrl = 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png';
+const defaultIcon = new L.Icon({
+  iconUrl: defaultIconUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
+
 const Map = () => {
-  const position = [36.13326, -5.45051]; // Set initial map coordinates
 
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      style={{ height: '400px', width: '100%' }}
-    >
+    <MapContainer center={[0, 0]} zoom={13} style={{ height: '400px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
-        <Popup>Guille's house</Popup>
-      </Marker>
+      
+        <Marker key={0} position={[36.13326, -5.45051]} icon={defaultIcon}>
+          <Popup>
+          </Popup>
+        </Marker>
     </MapContainer>
   );
 };
