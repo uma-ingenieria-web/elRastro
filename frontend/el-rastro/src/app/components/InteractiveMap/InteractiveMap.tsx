6ded8deceb901822b3b11836c4948a98e4284cbb
José Manuel <jmsanchezrico@gmail.com>
2023-12-08 22:42:20 +0100

    🍱 Add InteractiveMap component

---
+++ frontend/el-rastro/src/app/components/InteractiveMap/InteractiveMap.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { MapContainer, Marker, Popup, TileLayer, Tooltip, useMapEvents } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import "leaflet-defaulticon-compatibility"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+import { useState } from "react"
+
+export default function InteractiveMap({position, setPosition} : {position: [number, number], setPosition: Function}) {
+  function MyComponent() {
+    const map = useMapEvents({
+      click: (e) => {
+        const { lat, lng } = e.latlng;
+        setPosition([lat, lng]);
+      }
+    });
+    return null;
+  }
+
+
+  return <MapContainer center={position} scrollWheelZoom={true} zoom={13} className="w-screen h-80" >
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    <MyComponent>
+    </MyComponent>
+  </MapContainer>
+}
---
+++ frontend/el-rastro/src/app/components/InteractiveMap/index.ts
@@ -0,0 +1,11 @@
+
+import dynamic from 'next/dynamic';
+
+const InteractiveMap = dynamic(
+  () => import('./InteractiveMap'),
+  {
+    ssr: false,
+  }
+);
+
+export default InteractiveMap;
