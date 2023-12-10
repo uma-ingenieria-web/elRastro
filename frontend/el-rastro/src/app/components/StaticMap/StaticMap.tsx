b1d87e68e8f5a0480a35995b078db2e4cb15ccd8
PabloFS <pablofs02@tutanota.com>
2023-12-10 16:59:55 +0100

    🍁 Map z-index fixed

--- frontend/el-rastro/src/app/components/StaticMap/StaticMap.tsx
+++ frontend/el-rastro/src/app/components/StaticMap/StaticMap.tsx
@@ -6,7 +6,7 @@ import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 
 export default function StaticMap({position, popup} : {position: [number, number], popup: string}) {
-  return <MapContainer center={position} scrollWheelZoom={true} zoom={13} className="h-80" >
+  return <MapContainer center={position} scrollWheelZoom={true} zoom={13} className="h-80" style={{ zIndex: '0' }}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
