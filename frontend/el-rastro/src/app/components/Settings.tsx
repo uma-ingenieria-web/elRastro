9c800bc7c3b5f6e7242b9a4c47d7c6f6d63aa94b
José Manuel <jmsanchezrico@gmail.com>
2023-12-21 16:38:04 +0100

    ✨ Added "My products" link on Settings

--- frontend/el-rastro/src/app/components/Settings.tsx
+++ frontend/el-rastro/src/app/components/Settings.tsx
@@ -7,11 +7,16 @@ const Settings = ({ id }: { id: string }) => {
     const { data: session } = useSession();
 
     if (session?.user?.id === id)
-        return (<div className="flex flex-col justify-center mt-4">
-            <Link href={`/user/settings/new-photo`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2">
+        return (<div className="flex flex-col justify-center mt-4 gap-3">
+            <Link href={`/product/owner/${session?.user?.id}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                My products
+            </Link>
+            <hr/>
+            <h2 className="text-2xl font-bold text-black">Settings</h2>
+            <Link href={`/user/settings/new-photo`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Add new photo
             </Link>
-            <Link href={`/user/settings/change-name`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2">
+            <Link href={`/user/settings/change-name`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Change username
             </Link>
             <Link href={`/user/settings/change-location`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
