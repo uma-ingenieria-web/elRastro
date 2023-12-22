91ce3c4f97d54e407372ab70a141ef7a31b24295
PabloFS <pablofs02@tutanota.com>
2023-12-22 14:17:08 +0100

    😅 Rating with ternary

--- frontend/el-rastro/src/app/product/[id]/page.tsx
+++ frontend/el-rastro/src/app/product/[id]/page.tsx
@@ -442,11 +442,9 @@ function Product({ params }: { params: { id: string } }) {
                         <div className="mb-4">
                             {userId &&
                                 closed && userId !== product?.owner._id &&
-                                ((rating != 0 && (
-                                    <>
-                                        <p>Rated with {rating} ⭐</p>
-                                    </>
-                                )) || (
+                                (!rating ? (
+                                    <p>Rated with {rating} ⭐</p>
+                                ) : (
                                     <>
                                         <p className="mb-2">⭐ Rate your interaction with this sell</p>
                                         {!validRating && (
