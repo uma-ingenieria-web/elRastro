97bb665f1371e467d86a8ca7981de331d60c6f67
José Manuel <jmsanchezrico@gmail.com>
2023-12-22 10:45:42 +0100

    🚚 Update link in Closed component to /product

--- frontend/el-rastro/src/app/closed.tsx
+++ frontend/el-rastro/src/app/closed.tsx
@@ -7,7 +7,7 @@ export default function Closed() {
             <div className="flex flex-col justify-center  text-center">
                 <h2 className="text-4xl font-bold text-white text-center">Bid closed.</h2>
                 <p className="text-white mb-5">This product&apos;s auction has ended and you did not win.</p>
-                <Link href="/" className="bg-white hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
+                <Link href="/product" className="bg-white hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
                     Return Home
                 </Link>
             </div>
